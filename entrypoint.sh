39e0f628b31ae76f255e3ffc50b2f1f42b7359ca
konyogony <me@konyogony.dev>
2025-11-05 20:11:57 +0700

    Try better placeholders [DASH]

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,10 @@ find ./.next -type f \( -name "*.js" -o -name "*.css" -o -name "*.html" \) -prin
     sed -i "s|__NEXT_PUBLIC_FRONTEND_URL__|${NEXT_PUBLIC_FRONTEND_URL}|g" "$file"
     sed -i "s|__NEXT_PUBLIC_APP_NAME__|${NEXT_PUBLIC_APP_NAME}|g" "$file"
     sed -i "s|__NEXT_PUBLIC_UPLOAD_LIMIT_BYTES__|${NEXT_PUBLIC_UPLOAD_LIMIT_BYTES}|g" "$file"
-    sed -i "s|\"__TIERS_JSON__\"|${TIERS_JSON_SINGLE_LINE}|g" "$file"
-    sed -i "s|\"__NEXT_PUBLIC_FOOTER_LINKS__\"|${NEXT_PUBLIC_FOOTER_LINKS_SINGLE_LINE}|g" "$file"
-    sed -i "s|\"__NEXT_PUBLIC_NAVBAR_LINKS__\"|${NEXT_PUBLIC_NAVBAR_LINKS_SINGLE_LINE}|g" "$file"
+
+    sed -i "s|\"TIERS_JSON\":\[\]|\"TIERS_JSON\":${TIERS_JSON_SINGLE_LINE}|g" "$file"
+    sed -i "s|\"NEXT_PUBLIC_FOOTER_LINKS\":\{\}|\"NEXT_PUBLIC_FOOTER_LINKS\":${NEXT_PUBLIC_FOOTER_LINKS_SINGLE_LINE}|g" "$file"
+    sed -i "s|\"NEXT_PUBLIC_NAVBAR_LINKS\":\[\]|\"NEXT_PUBLIC_NAVBAR_LINKS\":${NEXT_PUBLIC_NAVBAR_LINKS_SINGLE_LINE}|g" "$file"
 done
 
 exec "$@"
